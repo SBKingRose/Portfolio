038f540133db710ac74db56f53c79e3abf22cb32
sbhaskar <sbhaskar@woundtech.net>
2025-10-01 16:03:12 +0530

    commented gsap

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { gsap } from "gsap";
+// import { gsap } from "gsap";
 import App from "./App.tsx";
 import "./index.css";
 
